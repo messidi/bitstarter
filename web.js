1ba40a40f1593ea08aaa239e2082b7c671e4445f
messidi <mohamed.elmessidi@gmail.com>
2013-07-12 10:22:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -7,10 +7,10 @@ var fs = require('fs');
 app.get('/', function(request, response) {
 
 
-var Buffer1 = new Buffer(fs.readFileSync('./index.html'));
-var k = Buffer1.toString("utf-8",0,Buffer1.length);
+var Buffer1 = new Buffer(fs.readFileSync('./index.html','utf8'));
+var k = Buffer1.toString('utf8',0,Buffer1.length);
     
-response.send('$k');
+response.send(k);
 });
 
 var port = process.env.PORT || 5000;
