346510e70b66211b37ebc75a2b97d8251c9ae379
messidi <mohamed.elmessidi@gmail.com>
2013-07-12 01:01:06 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var Buffer = require('buffer');
+var fs = require('fs');
 
-var app = express.createServer(express.logger());
+ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readFileSync('index.html');
+  response.send(Buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
